f603e3f1c3e1b93111cae23ee603448e9bf7dc08
TheXXne <jun87250@gmail.com>
2023-07-31 13:56:11 +0900

    infinite_scroll done

--- src/App.jsx
+++ src/App.jsx
@@ -41,34 +41,38 @@ function App() {
             label="Find the items you're looking for"
           />
         </div>
-        <div className="filterItem">
-          <span className="pricing-option">Pricing Options</span>
-          <div>
-            <input id={PricingOption.PAID} type="checkbox" 
-              onChange={(e)=>{
-                checkedOptionHandler(e.currentTarget.checked, PricingOption.PAID)
-              }}
-              checked={checkedOption.includes(PricingOption.PAID) ? true : false}
-            />
-            <span>Paid</span> 
+        <div>
+          <div className="filterItem">
+            <span className="pricing-option">Pricing Options</span>
+            <div>
+              <input id={PricingOption.PAID} type="checkbox" 
+                onChange={(e)=>{
+                  checkedOptionHandler(e.currentTarget.checked, PricingOption.PAID)
+                }}
+                checked={checkedOption.includes(PricingOption.PAID) ? true : false}
+              />
+              <span>Paid</span> 
+            </div>
+            <div>
+              <input id={PricingOption.FREE} type="checkbox" 
+                onChange={(e)=>{
+                  checkedOptionHandler(e.currentTarget.checked, PricingOption.FREE)
+                }}
+                checked={checkedOption.includes(PricingOption.FREE) ? true : false}
+              />
+              <span>Free</span> 
+            </div>
+            <div>
+              <input id={PricingOption.VIEW_ONLY} type="checkbox" 
+                onChange={(e)=>{
+                  checkedOptionHandler(e.currentTarget.checked, PricingOption.VIEW_ONLY)
+                }}
+                checked={checkedOption.includes(PricingOption.VIEW_ONLY) ? true : false}
+              />
+              <span>View Only</span>
+            </div>
           </div>
-          <div>
-            <input id={PricingOption.FREE} type="checkbox" 
-              onChange={(e)=>{
-                checkedOptionHandler(e.currentTarget.checked, PricingOption.FREE)
-              }}
-              checked={checkedOption.includes(PricingOption.FREE) ? true : false}
-            />
-            <span>Free</span> 
-          </div>
-          <div>
-            <input id={PricingOption.VIEW_ONLY} type="checkbox" 
-              onChange={(e)=>{
-                checkedOptionHandler(e.currentTarget.checked, PricingOption.VIEW_ONLY)
-              }}
-              checked={checkedOption.includes(PricingOption.VIEW_ONLY) ? true : false}
-            />
-            <span>View Only</span>
+          <div className="reset-div">
             <button className="reset-btn" onClick={optionResetHandler}>Reset</button>
           </div>
         </div>
